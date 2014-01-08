19fa268460d0b5e11d72582c1017ce945379a58e
Julio C. Menendez <hello@juliomenendez.com>
2014-01-08 14:13:30 -0700

    Fixes whitespace.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,29 @@
 module.exports = function (grunt) {
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-
-grunt.initConfig({
-    transpile: {
-        browser: {
-            type: 'amd',
-            files: [{
-                expand: true,
-                cwd: 'source/',
-                src: ['**/*.js'],
-                dest: 'tmp/'
-            }]
-        }
-    },
-    concat: {
-        browser: {
-            dest: 'dist/mobilerider-client.js'
+    grunt.initConfig({
+        transpile: {
+            browser: {
+                type: 'amd',
+                files: [{
+                    expand: true,
+                    cwd: 'source/',
+                    src: ['**/*.js'],
+                    dest: 'tmp/'
+                }]
+            }
+        },
+        concat: {
+            browser: {
                 src: [
                     'node_modules/es6-promise/dist/promise-*.amd.js',
                     'tmp/**/*.js'
                 ],
+                dest: 'dist/mobilerider-client.js'
+            }
         }
-    }
-});
+    });
 
-grunt.registerTask('build', ['transpile:browser', 'concat:browser']);
+    grunt.registerTask('build', ['transpile:browser', 'concat:browser']);
 
 };
